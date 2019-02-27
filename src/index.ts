cc810f42b645216774ed79e7fc0f4a56ae957d44
Anthony Freda <anthonyfreda323@gmail.com>
2019-02-27 16:30:55 -0500

    removed some stuff

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import logger, { stream } from './logger'
 import routes from './routes'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const { PORT } = process.env
@@ -14,6 +17,11 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('combined', { stream }))
 app.use('/api', routes)
+
+/** 404 */
+app.use((_, res) => {
+  res.status(404).json({ error: 'Route does not exist' })
+})
 
 app.listen(PORT, () => {
   logger.info(`App up on ${PORT}`)
